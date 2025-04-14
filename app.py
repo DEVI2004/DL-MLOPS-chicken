8bd73966ae2d5383214194dbd8604de27f56c293
DEVI2004 <devirishitha184@gmail.com>
2025-04-14 19:04:48 +0530

    New commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 from chickendisease.utils.common import decodeImage
 from chickendisease.pipeline.predict import PredictionPipeline
 
-
+#initalize flask
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
@@ -44,3 +44,6 @@ def predictRoute():
 if __name__ == "__main__":
     clApp = ClientApp()
     app.run(host='0.0.0.0', port=8080) #for AWS
+
+
+#set PYTHONPATH=src && python app.py
